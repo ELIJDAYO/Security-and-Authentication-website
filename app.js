d8b97336fe1f9be9d4cc7c9a41e0691f19a7d6a3
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-07 16:38:05 +0800

    Register user with username and password

--- app.js
+++ app.js
@@ -14,8 +14,18 @@ app.use(
     extended: true,
   })
 );
-// use public golder to store static file like css and img
+
+// use public folder to store static file like css and img
 app.use(express.static('public'));
+
+mongoose.connect('mongodb://127.0.0.1:27017/udemyUserDB', {
+  useNewUrlParser: true,
+});
+const userSchema = {
+  email: String,
+  password: String,
+};
+const User = new mongoose.model('User', userSchema);
 
 //TODO
 app.get('/', function (req, res) {
@@ -27,7 +37,37 @@ app.get('/login', function (req, res) {
 app.get('/register', function (req, res) {
   res.render('register');
 });
+app.post('/register', function (req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser
+    .save()
+    .then((result) => {
+      console.log(result);
+      res.render('secrets');
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+app.post('/login', (req, res) => {
+  const username = req.body.username;
+  const pass = req.body.password;
 
+  User.findOne({ email: username }).then((foundUser) => {
+    if (foundUser) {
+      if (foundUser.password == pass) {
+        res.render('secrets');
+      } else {
+        res.send('wrong password');
+      }
+    } else {
+      res.send('user not found');
+    }
+  });
+});
 app.listen(3000, function () {
   console.log('Server started on port 3000');
 });
