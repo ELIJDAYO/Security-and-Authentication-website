6737798c7221d68322886e98232c93821483f599
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-07 15:22:41 +0800

    set-up website

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 //jshint esversion:6
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+const app = express();
+// set templating engine
+app.set('view engine', 'ejs');
+// to parse the request
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+// use public golder to store static file like css and img
+app.use(express.static('public'));
+
+//TODO
+app.get('/', function (req, res) {
+  res.render('home');
+});
+app.get('/login', function (req, res) {
+  res.render('login');
+});
+app.get('/register', function (req, res) {
+  res.render('register');
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000');
+});
